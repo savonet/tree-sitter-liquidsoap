d74229e92dd2ffa0b43e23bcc0d99b1a86b11eeb
Romain Beauxis <toots@rastageeks.org>
2023-10-01 10:11:19 +0200

    exec

--- tests/run.sh
+++ tests/run.sh
@@ -9,4 +9,4 @@ cd "${BASE_DIR}/tests"
 rm -rf liquidsoap
 git clone --depth=1 https://github.com/savonet/liquidsoap.git
 cd "${BASE_DIR}"
-tree-sitter parse -q -s tests/liquidsoap/src/libs/**/*.liq tests/liquidsoap/tests/**/*.liq
+npm exec tree-sitter -- parse -q -s tests/liquidsoap/src/libs/**/*.liq tests/liquidsoap/tests/**/*.liq
